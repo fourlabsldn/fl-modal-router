6e0c241044c5f974d41ab9f921d453504d2108f9
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-03-24 12:33:02 +0000

    Add utils.showModalFromState

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,13 @@ var utils = (function utils() {  // eslint-disable-line
     return (noRemoteUrl) ? null : targetUrl;
   }
 
+  function showModalFromState(state) {
+    console.log('Imagine I am showing the modal with url ' + state.modalUrl);
+    return;
+  }
+
   return {
     getTargetUrl: getTargetUrl,
+    showModalFromState: showModalFromState,
   };
 }());
