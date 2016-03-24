0bd077f5e1b9be6eaed53521101f9be0e40e1cd5
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-03-24 15:11:39 +0000

    Add function to check whether modal is showing

--- src/utils.js
+++ src/utils.js
@@ -6,24 +6,42 @@ var utils = (function utils() {  // eslint-disable-line
     return (noRemoteUrl) ? null : targetUrl;
   }
 
-  function showModalFromState(state) {
+  function toggleModalFromState(state, showHide) {
     if (!state || !state.targetModal) {
       return;
     }
 
     var target = document.querySelector(state.targetModal);
     if (!target) {
-      throw new Error('showModalFromState: No modal found with ' +
-            state.targetModal);
+      throw new Error('toggleModalFromState: No modal found with ' +
+      state.targetModal);
     }
 
-    $(target).modal('show');
+    $(target).modal(showHide);
     console.log('Imagine I am showing the modal with url ' + state.modalUrl);
-    return;
+  }
+  function showModalFromState(state) {
+    toggleModalFromState(state, 'show');
+  }
+
+  function hideModalFromState(state) {
+    toggleModalFromState(state, 'hide');
+  }
+
+  function modalFromStateIsShowing(state) {
+    var modalIsShowing = false;
+    var modal;
+    if (!state) { return false; }
+    var modalQueryString = state.targetModal;
+    modal = document.querySelector(modalQueryString);
+    modalIsShowing = modal.classList.contains('in');
+    return modalIsShowing;
   }
 
   return {
     getTargetUrl: getTargetUrl,
     showModalFromState: showModalFromState,
+    hideModalFromState: hideModalFromState,
+    modalFromStateIsShowing: modalFromStateIsShowing,
   };
 }());
